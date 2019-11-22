3be2b90fdec0e0bd709c99f03a4aae23a8cccf85
1170522 <57870699+1170522@users.noreply.github.com>
2019-11-22 10:17:46 +0000

    init

---
+++ gestao-encomendas-ui/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://media.giphy.com/media/3ohhwvOnBaE8TtyBaw/giphy.gif" className="App-logo" alt="gif" />
+        ⚠ UI da Gestão de encomendas por implementar ⚠
+      </header>
+    </div>
+  );
+}
+
+export default App;
